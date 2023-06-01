2c1cb1378af7f5f95e06bd915c8ee28805d69556
rifaldibpn2 <rifaldibpn2@gmail.com>
2023-06-01 20:04:58 +0700

    API

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
-	"github.com/indrabpn12/Rest_API_Test.git/controllers/productController"
 	"github.com/indrabpn12/Rest_API_Test.git/models"
 )
 
